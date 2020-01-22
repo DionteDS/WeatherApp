d50ab5c897f8a0c99abec268b2de3e0dfc9244ea
Dionte <Dionte_sb@yahoo.com>
2020-01-21 18:11:13 -0800

    Implemented the search bar to search for a city weather.

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -12,15 +12,61 @@ import SwiftyJSON
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherStatus: UILabel!
+    @IBOutlet weak var weatherTemp: UILabel!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windDirection: UILabel!
     
-
+    let baseURL = "http://api.weatherstack.com/current"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Weather App"
+        
+        searchBar.placeholder = "Enter city"
+        
+    }
+    
+    func setQuery(city: String) {
+        
+        let params: [String: String] = ["access_key": APIKEY, "query": "San Francisco", "units": "f"]
+        
+        fetchData(url: baseURL, parameters: params)
+    }
+    
+    func fetchData(url: String, parameters: [String: String]) {
+        
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            response in
+            
+            if response.result.isSuccess {
+                print("Got data")
+            }
+        }
         
     }
 
 
 }
 
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        setQuery(city: searchBar.text!)
+        
+        DispatchQueue.main.async {
+            searchBar.resignFirstResponder()
+        }
+        
+    }
+    
+}
